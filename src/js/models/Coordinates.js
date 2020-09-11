bb3930780f46029e66c3ed636dd6acabe1b621e3
unknown <mcyoungerman@gmail.com>
2020-09-11 09:57:10 -0500

    Add map algorithm

--- src/js/models/Coordinates.js
+++
@@ -1,5 +0,0 @@
-function characterPosition(character) {
-    // get character's current x and y coordinates
-    let currX = character.x;
-    let currY = character.y;
-}
--- src/js/views/gameStateView.js
+++ src/js/views/gameStateView.js
@@ -10,3 +10,23 @@ function createBackground() {
     background.style.position = 'absolute';
 }
 
+function drawMap() {
+    for (let c = 0; c < map.cols; c++) {
+        for (let r = 0; r < map.rows; r++) {
+          let tile = map.getTile(c, r);
+          if (tile !== 0) { // 0 => empty tile
+            context.drawImage(
+              tileAtlas, // image
+              (tile - 1) * map.tsize, // source x
+              0, // source y
+              map.tsize, // source width
+              map.tsize, // source height
+              c * map.tsize, // target x
+              r * map.tsize, // target y
+              map.tsize, // target width
+              map.tsize // target height
+            );
+          }
+        }
+      }
+}
