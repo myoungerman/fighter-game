b56d65cbf681ecde374ab420536896dd6aa4dfba
unknown <mcyoungerman@gmail.com>
2020-09-12 23:54:55 -0500

    Add algorithm for generating the map

--- src/js/views/gameStateView.js
+++ src/js/views/gameStateView.js
@@ -1,8 +1,10 @@
-export { createBackground };
+export { drawBackground, drawMap };
 
 const background = document.getElementById('background');
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext("2d");
 
-function createBackground() {
+function drawBackground() {
     let swampBackground = new Image();
     swampBackground.src = "../../../img/map/background/background.png";
     background.src = swampBackground.src;
@@ -10,15 +12,18 @@ function createBackground() {
     background.style.position = 'absolute';
 }
 
-function drawMap() {
+function drawMap(map) {
+    let tileAtlas = new Image();
+    tileAtlas.src = "../../../img/map/tiles/tileset.png";
+
     for (let c = 0; c < map.cols; c++) {
         for (let r = 0; r < map.rows; r++) {
           let tile = map.getTile(c, r);
           if (tile !== 0) { // 0 => empty tile
-            context.drawImage(
+            ctx.drawImage(
               tileAtlas, // image
               (tile - 1) * map.tsize, // source x
-              0, // source y
+              tile.sourceY, // source y
               map.tsize, // source width
               map.tsize, // source height
               c * map.tsize, // target x
@@ -29,4 +34,10 @@ function drawMap() {
           }
         }
       }
+      console.log('drew map');
 }
+// adjust the source y based on what row we're in.
+// if tile index is > 18 but < than 36 (so second row), source y is 32. max source y is 160
+// round the tile index to the lowest closest multiple of 18 (E.g. 36)
+// 36 / the row (2) would give us the source y, which we oculd increment by 32
+// get result of. we could multiply 32 * the row number to get the source y
