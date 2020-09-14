43a7ce81a54dc98a86570c361cc2cffe279cbe28
unknown <mcyoungerman@gmail.com>
2020-09-13 23:34:05 -0500

    Generate map

--- src/js/models/GameState.js
+++ src/js/models/GameState.js
@@ -1,12 +1,8 @@
 export { loadMap };
 
 function loadMap() {
-    let tileSet = new Image();
-    tileSet.src = "../../../img/map/tiles/tileset.png";
-
-    // background img is 18 tiles across and 10 tiles high. Each tile is 32x32.
     let map = {
-        cols: 18,
+        cols: 18, // Swamp background img is 18 tiles across and 10 tiles high
         rows: 10,
         tsize: 32,
         tiles: [
@@ -14,52 +10,57 @@ function loadMap() {
           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-          0, 0, 0, 0, 0, 0, 31, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 31, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
           33, 33, 34, 0, 0, 0, 0, 0, 32, 34, 0, 0, 0, 0, 0, 0, 0, 0,
           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 10, 60, 2, 2,
           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 12, 12, 12, 12,
           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 12, 12, 12, 12,
-          20, 10, 3, 0, 0, 47, 2, 2, 20, 3, 0, 0, 16, 12, 12, 12, 12, 12
+          20, 10, 3, 0, 0, 47, 2, 2, 20, 3, 0, 0, 0, 16, 12, 12, 12, 12
         ],
         getTile: function(col, row) {
           let tile = this.tiles[row * map.cols + col];
           let sourceY = 0;
  
+        let remainder = 0;
+        let sourceX = 0;
+
         /*
-            if getTile returns 1-10, source y is 0
-            11-20, source y is 32
-            21-30, source y is 64
-            31-40, source y is 96
-            41-50, source y is 128
-            51-60, source y is 160
+        Calculate x coord of current tile by finding the first tile in its column in the tileset image 
+        */
+        if (tile > 10) { // Only needed if we aren't already in the first row of tiles
+            remainder = tile % 10;
+            if (remainder === 0) { // If remainder is 0, this tile is in column 10
+                remainder = 10; 
+            }
+            sourceX = (remainder * 32) - 32; 
+        } else { // The first row of tiles
+            sourceX = (tile * 32) - 32;
+        }
+
+        /*
+        Calculate source y of current tile. The tileset image has 6 rows, with 10 tiles per row
         */
 
-            if (tile >= 11 && tile <= 20) {
+        if (tile >= 11 && tile <= 20) {
             sourceY = 32;
-            } else if (tile >= 21 && tile <= 30) {
+        } else if (tile >= 21 && tile <= 30) {
             sourceY = 64;
-            } else if (tile >= 31 && tile <= 40) {
+        } else if (tile >= 31 && tile <= 40) {
             sourceY = 96;
-            } else if (tile >= 41 && tile <= 50) {
-                sourceY = 128;
-            } else if (tile >= 51 && tile <= 60) {
-                sourceY = 160;
-            } else {
-                sourceY = 0;
-            }
+        } else if (tile >= 41 && tile <= 50) {
+            sourceY = 128;
+        } else if (tile >= 51 && tile <= 60) {
+            sourceY = 160;
+        } else {
+            sourceY = 0;
+        }
 
-          return {
-              tile: tile,
-              sourceY: sourceY
-          };
+        return {
+            tile: tile,
+            sourceX: sourceX,
+            sourceY: sourceY
+        };
         }
     }
     return map;
 }
-
-/*
-tileSet is 10 tiles wide (320px) and 6 tiles high (192px)
-
-tile is 1, draws tile from (0,0).
-tile is 10, draws tile from (288, 0)
-*/
